e3f000d719ca63f5f5322a57e379a94765587954
Przemyslaw Pogorzelec <29803615+scrhicks@users.noreply.github.com>
2022-05-19 14:07:35 +0200

    Fixed outputs

--- outputs.tf
+++ outputs.tf
@@ -11,25 +11,25 @@ output "iam_service_linked_roles" {
 ##########################################################################
 output "elasticsearch_domain_arn" {
   description = "ARN of the domain"
-  value       = try(module.elasticsearch_domain[0].this.arn, "")
+  value       = try(module.elasticsearch_domain[0].arn, "")
 }
 
 output "elasticsearch_domain_id" {
   description = "Unique identifier for the domain"
-  value       = try(module.elasticsearch_domain[0].this.domain_id, "")
+  value       = try(module.elasticsearch_domain[0].domain_id, "")
 }
 
 output "elasticsearch_domain_name" {
   description = "Name of the Elasticsearch domain"
-  value       = try(module.elasticsearch_domain[0].this.domain_name, "")
+  value       = try(module.elasticsearch_domain[0].domain_name, "")
 }
 
 output "elasticsearch_domain_endpoint" {
   description = "Domain-specific endpoint used to submit index, search, and data upload requests"
-  value       = try(module.elasticsearch_domain[0].this.endpoint, "")
+  value       = try(module.elasticsearch_domain[0].endpoint, "")
 }
 
 output "elasticsearch_domain_kibana_endpoint" {
   description = "Domain-specific endpoint for kibana without https scheme"
-  value       = try(module.elasticsearch_domain[0].this._kibana_endpoint, "")
+  value       = try(module.elasticsearch_domain[0].kibana_endpoint, "")
 }
